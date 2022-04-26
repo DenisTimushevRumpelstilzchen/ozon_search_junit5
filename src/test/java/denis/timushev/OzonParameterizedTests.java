8204e3a3de5092b429f1c5e4856df6722b6ce859
Your NameDenis <tdv14011984@gmail.com>
2022-04-26 18:20:24 +0300

    add test case

--- src/test/java/denis/timushev/OzonParameterizedTests.java
+++ src/test/java/denis/timushev/OzonParameterizedTests.java
@@ -5,18 +5,17 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 
 public class OzonParameterizedTests {
-
     @BeforeAll
     static void setUp() {
         Configuration.browserSize = "1920x1080";
     }
-
     @ValueSource(strings = {
             "Nokia",
             "Xiaomi"
@@ -28,6 +27,19 @@ public class OzonParameterizedTests {
         $("[type=submit]").click();
         $$(".ri5")
                 .find(Condition.text(testData))
+                .shouldBe(Condition.visible);
+    }
+    @CsvSource(value = {
+            "Nokia, 105 SS",
+            "Xiaomi, Mijia Stainless Steel Nail Clippers (MJZJD002QW)"
+    })
+    @ParameterizedTest(name = "проверка поиска продукции {0} в магазине ozon, ожидаем {1}")
+    void ozonSearchComplexTest(String testData, String expectedResult) {
+        Selenide.open("https://www.ozon.ru");
+        $("[name=text]").setValue(testData);
+        $("[type=submit]").click();
+        $$(".ri5")
+                .find(Condition.text(expectedResult))
                 .shouldBe(Condition.visible);
     }
 }
